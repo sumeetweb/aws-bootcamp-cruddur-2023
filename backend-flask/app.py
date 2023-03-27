58c854d4daa9f9f552a350c473160dd7f9b738df
sumeetweb <25149022+sumeetweb@users.noreply.github.com>
2023-03-27 23:48:21 +0000

    add comments for honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,8 +32,12 @@ tracer = trace.get_tracer(__name__)
 
 # Initialize automatic instrumentation with Flask
 app = Flask(__name__)
+
+# Honeycomb
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
